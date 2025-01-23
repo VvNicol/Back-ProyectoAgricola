69e193b1509c1d3d032548d976ea9387ade518ac
nrojlla <csi23-nrojlla@colegioaltair.net>
2025-01-23 09:23:30 +0100

    Conexion y creacion de tablas JPA

--- src/main/java/agrilog/controladores/Inicio.java
+++ src/main/java/agrilog/controladores/Inicio.java
@@ -5,11 +5,13 @@ package agrilog.controladores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 /**
  * Inicio de la aplicacion spring
  */
 @SpringBootApplication
+@EntityScan(basePackages = "agrilog.modelos")
 public class Inicio {
 
 	public static void main(String[] args) {
