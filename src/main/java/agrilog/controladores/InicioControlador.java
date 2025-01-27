180ceba14013f042c60b6468d21fe70d55c0875c
vvale <rojasvillavalentina01@gmail.com>
2025-01-28 00:37:46 +0100

    CORS

--- src/main/java/agrilog/controladores/InicioControlador.java
+++ src/main/java/agrilog/controladores/InicioControlador.java
@@ -3,6 +3,7 @@ package agrilog.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import agrilog.modelos.UsuarioModelo;
 import agrilog.servicios.UsuarioInterfaz;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/inicio")
 public class InicioControlador {
